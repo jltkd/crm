e82e65e666e7758cfc2a8d933a20a0365a1b2562
jltkd <jl@turnkeydigital.com>
2021-01-26 15:48:51 -0600

    Moved status in company list

--- resources/views/livewire/company/show.blade.php
+++ resources/views/livewire/company/show.blade.php
@@ -22,20 +22,23 @@
                         <p class="text-sm font-medium text-gray-900">
                             {{ $company->company_name }}
                         </p>
+                        <p class="text-xs text-gray-400 truncate">
+                            {{ $company->phone_number }}
+                        </p>
                         @if($company->status === 'live')
-                            <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $company->status }}</span>
+                            <span class="absolute top-3 right-3 flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ $company->status }}</span>
                         @elseif($company->status === 'prospect')
-                            <span class="flex-shrink-0 inline-block px-2 py-0.5 text-yellow-800 text-xs font-medium bg-yellow-100 rounded-full">{{ $company->status }}</span>
+                            <span class="absolute top-3 right-3 flex-shrink-0 inline-block px-2 py-0.5 text-yellow-800 text-xs font-medium bg-yellow-100 rounded-full">{{ $company->status }}</span>
                         @else
-                            <span class="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-full">{{ $company->status }}</span>
+                            <span class="absolute top-3 right-3 flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-full">{{ $company->status }}</span>
                         @endif
                     </a>
                 </div>
-                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">
-                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-                  </svg>
-                </span>
+{{--                <span class="pointer-events-none absolute top-6 right-6 text-gray-300 group-hover:text-gray-400" aria-hidden="true">--}}
+{{--                  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">--}}
+{{--                    <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />--}}
+{{--                  </svg>--}}
+{{--                </span>--}}
             </div>
         @empty
             <div class="flex">
